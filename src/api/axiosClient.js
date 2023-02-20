72ef044dc326fc558f7de4907b7ef823852b50db
Nguyễn Quang Thao <nquangthao19072004@gmail.com>
2023-02-21 00:34:11 +0700

    update api

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 import tokenApi from './tokenApi';
 
 const axiosClient = axios.create({
-  // baseURL: 'https://vast-erin-tuna-suit.cyclic.app/api/v1/',
-  baseURL: 'http://localhost:3001/api/v1/',
+  baseURL: 'https://vast-erin-tuna-suit.cyclic.app/api/v1/',
+  // baseURL: 'http://localhost:3001/api/v1/',
   headers: {
     'content-type': 'application/json',
   },
